1557d728e2c094a7977eb30ab6d87e80918e64a4
Marshmellowon <leo503801@gmail.com>
2020-06-20 20:24:17 +0900

    finish

--- Generic_Alrorithm.py
+++ Generic_Alrorithm.py
@@ -2,7 +2,6 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import random as rn
-import numpy as np
 
 # 데이터 셋 찾음
 # MSE 오차율을 유전알고리즘으로 구하기
@@ -36,10 +35,12 @@ def nota(t, h):
 # 예측된 값으로 그래프 그리기
 def predicted(a, xVal):
     yArr = []
-    print("a", a)
-    print("xVal", xVal)
     for i in range(len(xVal)):
-        y = (a - 0.5) * xVal[i] + 50
+        # +80을 하여 임의로 상수 값의 예측을 대신하였다.
+        if a > 0:
+            y = -a * xVal[i] + 80
+        else:
+            y = a * xVal[i] + 80
         yArr.append(float(y))
     return yArr
 
@@ -78,11 +79,13 @@ def selection(Aarr):
     return sx
 
 
+# bin() format
 def binformat(x):
     binformat = '{0:>8}'.format(bin(x)).replace("b", "0").replace(" ", "0").replace("-", "0")
     return binformat
 
 
+# int to binary
 def int2Bin(str):
     binlist = []
     for i in range(4):
@@ -91,52 +94,53 @@ def int2Bin(str):
     return binlist
 
 
+# 교차 연산
 def crossover(arr):
     strarr = []
     for i in range(2):
         bita = str(arr[i])
         bitb = str(arr[i + 1])
 
-        strarr.append(bita[:4] + bitb[4:])
-        strarr.append(bitb[:4] + bita[4:])
+        strarr.append(bita[:5] + bitb[5:])
+        strarr.append(bitb[:5] + bita[5:])
 
     return strarr
 
 
+# 돌연변이 연산
 def invert(char):
     ran = rn.random()
     a = int(char, 2)  # 숫자
     abin = binformat(a)
     abinstr = str(abin)
     reta = 0
-
     for i in range(5):
-        p = 1 / 5
+        p = 1 / 50
         if ran < p:
             if abinstr[5:6] == "1":
-                reta = a & ~(2 << 3)
+                reta = a & ~(1 << 4)
 
             elif abinstr[5:6] == "0":
-                reta = a | (2 << 3)
+                reta = a | (1 << 4)
             return reta
         return a
 
 
+# 돌연변이 한 값 저장하기
 def mutation(mut):
     mutarr = []
-    mutarr2 = []
-    output = []
     mutint = list(map(str, mut))
     for i in range(4):
-        mutarr.append(float(-invert(mutint[i])))
-
+        mutinv = float(-invert(mutint[i]))
+        mutarr.append(mutinv)
     return mutarr
 
 
 # MSE 계산식
-def MSEnotation(yInit, yPredict):
-    MSEval = pow(yPredict - yInit, 2)
-    return MSEval
+def MSEexpression(yInit, yPredict):
+    MSEval = yPredict - yInit
+    powMSE = MSEval * MSEval
+    return powMSE
 
 
 # 오차율 구하기
@@ -146,27 +150,24 @@ def MSE(predict, tempdata):
     for i in range(4):
         for j in range(144):
             initnotation = predict[i] * tempdata[j]
-            yY = MSEnotation(arr2[i], initnotation)
+            yY = MSEexpression(arr2[j], initnotation)
             yYval.append(float(yY))
-            yMSE = 1 / 2000000 * sum(yYval)
+            yMSE = 1 / 265900 * sum(yYval)
         initgraph.append(float(yMSE))
-
     return initgraph
 
 
+# 기울기와 MSE값을 가진 리스트에서 최소 MSE의 기울기를 찾아낸다.
 def minx(x, list2):
     for lis in list2:
         if str(x) in lis:
             return float(lis[0])
 
 
-# TODO: 선정된 4개의 a와 랜덤한 기온값 대입 vs 초기 데이터와 비교(MSE) MSE그래프 그리기
-# TODO: 최소 오차율인 기울기로 랜덤한 기온값 대입하여 예측그래프 그리기
-# MSE = 1/n sum(Y'-Y)^2
-# 유전알고리즘 main---------
-tempmin = 5
+# 유전알고리즘 ---------main---------
+tempmin = 18
 tempmax = 35
-hummin = 10
+hummin = 20
 hummax = 95
 
 x = []
@@ -174,7 +175,9 @@ y = []
 list2 = []
 TempPred = []
 
+# 반복 100번
 for i in range(100):
+    # 각 함수들의 사용
     initreturn = init(tempmin, tempmax, hummin, hummax)
     selectionreturn = selection(initreturn)
     inttobinary = int2Bin(selectionreturn)
@@ -182,30 +185,37 @@ for i in range(100):
     mutationed = mutation(cross)
     MSEarr = MSE(mutationed, arr)
 
-    # print test
+    # print를 통해 원하는 값이 나오는지 확인
     print("---------------------")
     print("init: ", initreturn)
     print("selection:", selectionreturn)
     print("inttobin: ", inttobinary)
     print("cross: ", cross)
-    print(mutationed)
-    print(MSEarr)
+    print("mutationed: ", mutationed)
+    print("MSEarr:", MSEarr)
+    print("예측된 기울기:", min(mutationed))
+    print("-----------------------")
 
+    # x와 y에 예측된 기울기와 MSE값을 넣은다.
     x += mutationed
     y += MSEarr
 
-    list2.append([str(x[i]), str(y[i])])
+    # 예측된 기울기 값과 랜덤 온도로 그래프 예측하기위한 값
+    temperature = rn.randint(18, 35)
+    TempPred.append(temperature)
 
-    temperature = rn.randint(tempmin, tempmax)
-    TempPred.append(float(temperature))
+    # x와 y에 예측된 기울기와 MSE값을 넣은다.
+    for u in range(len(y)):
+        list2.append([str(x[i]), str(y[i])])
 
-    minX = minx(min(y), list2)
-    if minX == None:
-        print("none")
+# minx 함수에서 최소 MSE의 기울기를 알아낸다.
+minX = minx(min(y), list2)
+print("최소 MSE 기울기: ", minX)
 
-print("prit", minX)
+# 그래프 예측을 위한 계산(predicted 함수 사용)
 yPredict = predicted(minX, TempPred)
 
+# pandas library로 DataFrame 지정
 MSEGrapg = pd.DataFrame(
     {"aValue": x, "MSE": y}
 )
@@ -213,21 +223,27 @@ MSEGrapg = pd.DataFrame(
 temp = pd.DataFrame(
     {"temp": arr, "hum": arr2}
 )
+
 predictgraph = pd.DataFrame(
     {"temp2": TempPred, "hum2": yPredict}
 )
 
-print(list2)
-
+# 그래프 그리기
 plt.figure()
+# 기존 데이터의 그래프를 그린다
 plt.scatter(temp['temp'], temp['hum'], marker="o")
+# 예측된 기울기로 그래프를 예측한다.
 plt.scatter(predictgraph["temp2"], predictgraph["hum2"], marker="x")
 plt.xlabel('temperature')
 plt.ylabel('humedity')
 plt.savefig("initial.png")
 
 plt.figure()
+# MSE 평가지표 그래프
 plt.scatter(MSEGrapg['aValue'], MSEGrapg['MSE'], marker="o")
 plt.xlabel('inclination')
 plt.ylabel('MSE')
 plt.savefig("MSE.png")
+
+# 그래프 띄우기
+plt.show()
